52e09e112bb8925ada968d6fa9eff964f2103e6a
Bello Samuel <prinxsabelo@gmail.com>
2022-06-01 18:13:05 +0100

    ..

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -12,11 +12,11 @@ const Footer = () => {
             </p>
             <div className='flex space-x-4'>
                 <a href="https://www.linkedin.com/in/oyewumi-olukunle-10b07716a"
-                    className=' p-2 rounded-full'>
+                    target="_blank" className=' p-2 rounded-full'>
                     <FaLinkedin className='w-4 h-4' />
                 </a>
                 <a href="https://www.behance.net/olukunleoyewumi"
-                    className=' p-2 rounded-full'>
+                    target="_blank" className=' p-2 rounded-full'>
                     <FaBehance className='w-4 h-4' />
                 </a>
 
